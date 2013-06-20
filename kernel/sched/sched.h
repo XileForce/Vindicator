081de6c4a6d836fd12ef9180856714f1647b80cd
Alex Shi <alex.shi@intel.com>
2013-06-20 10:18:55 +0800

    sched/cfs_rq: Change atomic64_t removed_load to atomic_long_t
    Similar to runnable_load_avg, blocked_load_avg variable, long type is
    enough for removed_load in 64 bit or 32 bit machine.
    
    Then we avoid the expensive atomic64 operations on 32 bit machine.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    Reviewed-by: Paul Turner <pjt@google.com>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1371694737-29336-12-git-send-email-alex.shi@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -282,7 +282,7 @@ struct cfs_rq {
 	 * the FAIR_GROUP_SCHED case).
 	 */
 	unsigned long runnable_load_avg, blocked_load_avg;
-	atomic64_t decay_counter, removed_load;
+	atomic64_t decay_counter;
 	u64 last_decay;
 	atomic_long_t removed_load;
 
