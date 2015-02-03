0d9658635a0c95b1058564dd177f7b30b1860ab0
XileForce <rdaguiar360@gmail.com>
2015-02-03 15:32:37 -0800

    Add Wheatley Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -430,6 +430,9 @@ extern struct cpufreq_governor cpufreq_gov_conservativex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_wheatley)
 #endif
 
 /*********************************************************************
