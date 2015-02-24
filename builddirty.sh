6850915f77187f2f95f8b78a019a674e23bda97b
Iliyan Malchev <malchev@google.com>
2015-02-23 19:43:51 -0800

    power: add partial-resume framework
    Partial resume refers to the concept of not waking up userspace when the kernel
    comes out of suspend for certain types of events that we wish to discard.  An
    example is a network packet that can be disacarded in the kernel, or spurious
    wakeup event that we wish to ignore.  Partial resume allows drivers to register
    callbacks, one one hand, and provides hooks into the PM's suspend/resume
    mechanism, on the other.
    
    When a device resumes from suspend, the core suspend/resume code invokes
    partialresume to check to see if the set of wakeup interrupts all have
    matching handlers. If this is not the case, the PM subsystem can continue to
    resume as before.  If all of the wakeup sources have matching handlers, then
    those are invoked in turn (and can block), and if all of them reach consensus
    that the reason for the wakeup can be ignored, they say so to the PM subsystem,
    which goes right back into suspend.  This latter support is implemented in a
    separate change.
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    Change-Id: Id50940bb22a550b413412264508d259f7121d442
    
    Conflicts:
    	kernel/power/Kconfig

--- builddirty.sh
+++ builddirty.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sdir="/home/ryan/Kernels/Vindicator"
-udir="/home/ryan/Kernels/Vindicator/utils"
-outdir="/home/ryan/Kernels/Vindicator_Out"
+sdir="."
+udir="utils"
+outdir="../Vindicator_Out"
 device="shamu"
-cc="/home/ryan/arm-eabi-6.0/bin/arm-eabi-"
-filename="Vindicator.2.1.zip"
+cc="../arm-eabi-6.0/bin/arm-eabi-"
+filename="Vindicator.M.zip"
 
 compile() {
   export CROSS_COMPILE=$cc
@@ -14,7 +14,7 @@ compile() {
   export KBUILD_BUILD_USER=Xile
   export KBUILD_BUILD_HOST=Xile
   make vindicator_defconfig
-  make -j4
+  make -j24
 }
 
 ramdisk() {
@@ -26,16 +26,15 @@ ramdisk() {
   chmod 755 res res/images res/images/charger
   chmod 640 fstab.shamu
   find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
-  /bin/mkbootimg --kernel $sdir/arch/arm/boot/zImage-dtb  --ramdisk /tmp/ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M androidboot.selinux=permissive" --pagesize 2048 -o $outdir/boot.img
+  ~/.bin/mkbootimg --kernel ../arch/arm/boot/zImage-dtb  --ramdisk /tmp/ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M androidboot.selinux=permissive" -o $outdir/boot.img
 }
 
 zipit() {
-  cd $udir
   cp -f $outdir/boot.img zip/
   cd zip
   zip -r $outdir/$1 *
-  rm boot.img
-  cd $sdir
+  #rm boot.img
+  cd ..
 } 
 
 compile && ramdisk && zipit $filename 
