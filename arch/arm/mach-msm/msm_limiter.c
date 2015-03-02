64e878397461d6fdd96d9e18993ed9395f3fd36f
XileForce <rdaguiar360@gmail.com>
2015-03-01 16:51:33 -0800

    set min to 35mhz

--- arch/arm/mach-msm/msm_limiter.c
+++ arch/arm/mach-msm/msm_limiter.c
@@ -27,7 +27,7 @@
 #define DEFAULT_SUSPEND_DEFER_TIME	10 
 #define DEFAULT_SUSPEND_FREQUENCY	1728000
 #define DEFAULT_RESUME_FREQUENCY	2649600
-#define DEFAULT_MIN_FREQUENCY		300000
+#define DEFAULT_MIN_FREQUENCY		35800
 
 static unsigned int debug = 0;
 module_param_named(debug_mask, debug, uint, 0644);
