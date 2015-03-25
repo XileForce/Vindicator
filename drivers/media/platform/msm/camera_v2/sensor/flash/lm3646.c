409870692c7de032318f59d074b347f49f163443
XileForce <rdaguiar360@gmail.com>
2015-03-24 18:13:48 -0700

    Revert "use deferred initcalls"
    This reverts commit f8b940459c7159842cb98a65ba8b9e104cf736cd.

--- drivers/media/platform/msm/camera_v2/sensor/flash/lm3646.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/lm3646.c
@@ -181,7 +181,7 @@ static void __exit msm_flash_lm3646_exit_module(void)
 	return;
 }
 
-deferred_module_init(msm_flash_lm3646_init);
+module_init(msm_flash_lm3646_init);
 module_exit(msm_flash_lm3646_exit_module);
 MODULE_DESCRIPTION("LM3646 FLASH");
 MODULE_LICENSE("GPL v2");
