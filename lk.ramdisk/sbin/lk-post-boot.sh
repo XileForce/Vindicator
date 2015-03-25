320343d7cf1f558a1d24e242bc9d98ab14c61755
XileForce <rdaguiar360@gmail.com>
2015-03-24 22:06:28 -0700

    Disable MPDecision By Default. Mako Is Default Now

--- lk.ramdisk/sbin/lk-post-boot.sh
+++ lk.ramdisk/sbin/lk-post-boot.sh
@@ -34,6 +34,9 @@ chmod 755 /data/data/leankernel
 # init.d support
 /system/xbin/busybox run-parts /system/etc/init.d
 
+# Default MPDecision OFF!
+stop mpdecision
+
 #
 # lkconfig settings below
 #
