c0b1705d76b82d265f754675c96640c864952bce
XileForce <rdaguiar360@gmail.com>
2015-03-25 22:30:49 -0700

    Revert Deferred Init On BT Stuff

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -969,7 +969,7 @@ static void __exit bluesleep_exit(void)
 	remove_proc_entry("bluetooth", 0);
 }
 
-deferred_module_init(bluesleep_init);
+module_init(bluesleep_init);
 module_exit(bluesleep_exit);
 
 MODULE_DESCRIPTION("Bluetooth Sleep Mode Driver ver %s " VERSION);
