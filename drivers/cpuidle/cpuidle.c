a0f85796c42c601c1d4f77c836a3904885186cf4
XileForce <rdaguiar360@gmail.com>
2015-04-07 23:50:21 -0700

    Revert "cpuidle: Use wake_up_all_idle_cpus() to wake up all idle cpus"
    This reverts commit 3253c419e5b62e3f20471d50240388103e348ac4.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -196,7 +196,7 @@ void cpuidle_uninstall_idle_handler(void)
 {
 	if (enabled_devices) {
 		initialized = 0;
-		wake_up_all_idle_cpus();
+		kick_all_cpus_sync();
 	}
 }
 
@@ -535,6 +535,12 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
 
+#if 0
+static void smp_callback(void *v)
+{
+	/* we already woke the CPU up, nothing more to do */
+}
+#endif
 
 /*
  * This function gets called when a part of the kernel has a new latency
@@ -545,7 +551,15 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_all_idle_cpus();
+#if 0
+	/* when drivers request new latency requirement, it does not necessary
+	 * to immediately wake up another cpu by sending cross-cpu IPI, we can
+	 * consider the new latency to be taken into effect after next wakeup
+	 * from idle, this can save the unnecessary wakeup cost, and reduce the
+	 * risk that drivers may request latency in irq disabled context.
+	 */
+	smp_call_function(smp_callback, NULL, 1);
+#endif
 	return NOTIFY_OK;
 }
 
