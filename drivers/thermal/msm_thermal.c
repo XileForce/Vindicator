20043a76ba1a128b55329f0c1f5e6b614d0c2ea3
XileForce <rdaguiar360@gmail.com>
2015-04-19 19:50:18 -0700

    Thermal: Default to 70

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -53,7 +53,7 @@
 bool full_fm = true;
 module_param(full_fm, bool, 0644);
 
-unsigned int temp_threshold = 60;
+unsigned int temp_threshold = 70;
 module_param(temp_threshold, int, 0755);
 
 static struct msm_thermal_data msm_thermal_info;
