1ba108e3845270292c5926d2801261009248e095
XileForce <rdaguiar360@gmail.com>
2015-04-23 18:15:28 -0700

    Wakelocks: Reduce Wlan_ctrl, default wlan_rx to divisor of 5 and hsic to 6

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -171,7 +171,7 @@ static u32 vendor_oui = CONFIG_DHD_SET_RANDOM_MAC_VAL;
 #include <wl_android.h>
 #include <linux/moduleparam.h>
 
-static int wl_divide = 1;
+static int wl_divide = 5;
 module_param(wl_divide, int, 0644);
 
 /* Maximum STA per radio */
@@ -8018,7 +8018,7 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/wl_divide);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/4);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
